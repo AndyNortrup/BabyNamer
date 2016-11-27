f4ab67de81dc65b9b2f98c8d9e1096c82190cf5c
Andy Nortrup <andrew.nortrup@gmail.com>
2016-11-26 20:50:44 -0800

    Added a handler for the favicon.ico file.

--- app.go
+++ app.go
@@ -30,6 +30,7 @@ func init() {
 	r.HandleFunc("/", namesPage)
 	r.HandleFunc("/short-list", handleShortList)
 	r.HandleFunc("/settings", handleSettings)
+	r.HandleFunc("/favicon.ico", faviconHandler)
 	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("templates/css"))))
 	r.PathPrefix("/js/").Handler(http.StripPrefix("/js/", http.FileServer(http.Dir("templates/js"))))
 	r.PathPrefix("/fonts/").Handler(http.StripPrefix("/fonts/", http.FileServer(http.Dir("templates/fonts"))))
@@ -37,36 +38,6 @@ func init() {
 	http.Handle("/", r)
 }
 
-// func getNameHandler(w http.ResponseWriter, r *http.Request) {
-// 	defer r.Body.Close()
-// 	ctx := appengine.NewContext(r)
-// 	gen := NewNameGenerator(ctx)
-// 	gen.getName(w, r)
-// }
-//
-// func apiMakeDecision(w http.ResponseWriter, r *http.Request) {
-//
-// 	defer r.Body.Close()
-//
-// 	//Get the user's name
-// 	ctx := appengine.NewContext(r)
-// 	username := user.Current(ctx)
-//
-// 	name := mux.Vars(r)["name"]
-// 	var decision bool
-//
-// 	if mux.Vars(r)["choice"] == "yes" {
-// 		decision = true
-// 	} else {
-// 		decision = false
-// 	}
-//
-// 	log.Infof(ctx, "User: %v says %v to %v", username, decision, name)
-// 	err := updateNameRecommendations(name, username.Email, decision, ctx)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 	}
-//
-// 	log.Infof(ctx, "Requesting new name to return")
-// 	http.Redirect(w, r, "/?previous="+name, http.StatusSeeOther)
-// }
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "favicon.ico")
+}
